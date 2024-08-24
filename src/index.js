8547ba2b0e7b195250c8547129c39965682e4429
Bellangelo <aggelosbellos7@gmail.com>
2024-08-24 18:55:27 +0300

    Read keywords from a file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
 
 async function run() {
     try {
-        // Get the comment body from the GitHub event payload
-        const commentBody = github.context.payload.comment.body;
+        const language = 'en';
+        const keywords = JSON.parse(fs.readFileSync('./keywords/' + language + '.json', 'utf8'));
 
-        // Replace the keywords with the corresponding phrases
-        const updatedBody = commentBody.replace(':compliment:', '[ a nice compliment ]')
-            .replace(':insult:', '[ a bad insult ]');
+        let commentBody = github.context.payload.comment.body;
+
+        Object.keys(keywords).forEach((key) => {
+            commentBody = commentBody.replace(new RegExp(key, 'g'), function (match) {
+                const items = keywords[key];
+                return items[Math.floor(Math.random()*items.length)];
+            });
+        });
 
         // Set the updated body as output
-        core.setOutput('updated-body', updatedBody);
+        core.setOutput('updated-body', commentBody);
     } catch (error) {
         core.setFailed(`Action failed with error ${error}`);
     }
