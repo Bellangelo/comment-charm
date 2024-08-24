cd61e8fbcc3409537da52c32cd4f1420a41f36d6
Bellangelo <aggelosbellos7@gmail.com>
2024-08-24 19:48:03 +0300

    Use the language

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const Replacer = require('./Replacer.js');
 
 try {
     const commentBody = core.getInput('comment-body');
     const language = core.getInput('language');
 
-    core.setOutput('updated-body', (new Replacer()).replace(commentBody));
+    core.setOutput('updated-body', (new Replacer(language)).replace(commentBody));
 } catch (error) {
     core.setFailed(`Action failed with error ${error}`);
 }
